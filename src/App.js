592b22bdbea5170ae510752a0b9f6702f39e4211
Andreas Houben <andreas.houben@codecentric.de>
2021-04-16 15:00:52 +0200

    make nicer =)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import Main from "./pages/main";
+import ram from "./ram.png";
+import styled from "styled-components";
 
-function App() {
-  return <Main />;
-}
+const Background = styled.img`
+  height: 200px;
+  position: fixed;
+  bottom: 1vh;
+  right: 10px;
+`;
+
+const App = () => (
+  <>
+    <h1>Magnificent Rick & Morty Character Wheel</h1>
+    <Main />
+    <Background src={ram} />
+  </>
+);
 
 export default App;
