a39a57cabbe2df6eda1e1c582c4ed9c0aab2b67d
Andreas Houben <andreas.houben@codecentric.de>
2021-04-17 12:59:20 +0200

    add media queries

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import styled from "styled-components";
 
 const Background = styled.img`
   height: 200px;
-  position: fixed;
+  position: absolute;
   bottom: 1vh;
   right: 10px;
+  transition: bottom 0.1s,right 0.1s;
+  @media(max-width:1000px) and (max-height:850px){
+    right: -150px;
+    bottom: -200px;
+    transition: right 0.1s, bottom 0.1s;   
+ } 
 `;
 
 const App = () => (
-  <>
-    <h1>Magnificent Rick & Morty Character Wheel</h1>
-    <Main />
-    <Background src={ram} />
-  </>
+    <>
+        <h1>Magnificent Rick & Morty Character Wheel</h1>
+        <Main/>
+        <Background src={ram}/>
+    </>
 );
 
 export default App;
